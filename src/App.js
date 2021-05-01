a08b14d331cedbd23c3168dcf03ac5f12013923c
Hasib787 <mr.hasib45@gmail.com>
2021-05-01 16:25:50 +0600

    added more components

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ import {
   Route
 } from "react-router-dom";
 import './App.css';
+import Blog from "./components/Blog/Blog";
 import Home from "./components/Home/Home";
 import NotFound from "./components/NotFound/NotFound";
+import Projects from "./components/Projects/Projects";
+import Resume from "./components/Resume/Resume";
 
 function App() {
 
@@ -15,6 +18,18 @@ function App() {
       <Switch>
         <Route exact path="/">
             <Home />
+        </Route>
+        <Route path="/home">
+          <Home />
+        </Route>
+        <Route path="/projects">
+          <Projects />
+        </Route>
+        <Route path="/blog">
+          <Blog />
+        </Route>
+        <Route path="/resume">
+          <Resume />
         </Route>
         <Route path="*">
           <NotFound />
---
+++ src/components/Blog/Blog.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Blog = () => {
+    return (
+        <div>
+            <h3 style={{color:'orange'}}>Page are coming soon</h3>
+        </div>
+    );
+};
+
+export default Blog;
