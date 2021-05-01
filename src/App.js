deb555cee6eb3738400d5eee1ea467685a315a1a
Hasib787 <mr.hasib45@gmail.com>
2021-05-01 16:09:37 +0600

    Router are added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 import './App.css';
+import Home from "./components/Home/Home";
+import NotFound from "./components/NotFound/NotFound";
 
 function App() {
+
   return (
-    <div>
-     
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+            <Home />
+        </Route>
+        <Route path="*">
+          <NotFound />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
