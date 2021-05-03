0787264c08c8d48ed3246bd7e76914f45e102fa0
Hasib787 <mr.hasib45@gmail.com>
2021-05-04 03:27:14 +0600

    fixing some problems

--- src/components/Shared/HeaderNav/HeaderNav.js
+++ src/components/Shared/HeaderNav/HeaderNav.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import { Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import './HeaderNav.css';
 
 const HeaderNav = () => {
     return (
         <div>
             <Navbar className="s-navbar" collapseOnSelect expand="lg" bg="dark" variant="dark">
-                <Navbar.Brand className="fw-bold fs-3 logo" href="#home">Masud <span style={{color:'#00a1a7'}}>Rana</span></Navbar.Brand>
+            <Link to="/home" id="logo"><Navbar.Brand className="fw-bold fs-3" href="#home">Masud <span style={{color:'#00a1a7'}}>Rana</span></Navbar.Brand></Link>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="mr-auto nav-menu">
-                        <Nav.Link className="nav-title" href="/home" active>Home</Nav.Link>
+                    <Link to="/home" className="nav-title"><Nav.Link className="nav-title" href="#home" active>Home</Nav.Link></Link>
                         <Nav.Link  className="nav-title" href="#about">About</Nav.Link>
-                        <Nav.Link  className="nav-title" href="/projects">Projects</Nav.Link>
-                        <Nav.Link  className="nav-title" href="/blog">Blog</Nav.Link>
+                        <Link to="/projects" className="nav-title"><Nav.Link  className="nav-title" href="#projects">Projects</Nav.Link></Link>
+                        <Link to="/blog" className="nav-title"><Nav.Link  className="nav-title"  href="#blog">Blog</Nav.Link></Link>
                         <Nav.Link  className="nav-title" href="#contact">Contact</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
