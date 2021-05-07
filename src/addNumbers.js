938c1018e991042dd8b0f933c55b339868a60cb0
Hasib787 <mr.hasib45@gmail.com>
2021-05-07 21:27:41 +0600

    added a component and math.ceil function

--- src/addNumbers.js
+++
@@ -1,7 +0,0 @@
-const addSomeNumber = (number1, number2) => {
-  let  total = number1+ number2;
-    return total;
-}
-
-const result = addSomeNumber(55,20);
-console.log(result);
---
+++ src/math.ceil.js
@@ -0,0 +1,11 @@
+console.log(Math.ceil(.80));
+// expected output: 1
+
+console.log(Math.ceil(7));
+// expected output: 7
+
+console.log(Math.ceil(12.004));
+// expected output: 13
+
+console.log(Math.ceil(-7.004));
+// expected output: -7
